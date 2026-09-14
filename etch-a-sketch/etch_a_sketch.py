a06f5ef5519e760dc1981fbf0d9b9850a1af226a
smithEliF <smith.eli.frank@gmail.com>
2026-09-14 17:19:35 -0400

    finished etch-a-sketch

---
+++ etch-a-sketch/etch_a_sketch.py
@@ -0,0 +1,30 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+tim.pencolor("black")
+screen = Screen()
+
+def move_forward():
+    tim.forward(10)
+
+def move_backward():
+    tim.backward(10)
+
+def turn_left():
+    tim.left(10)
+
+def turn_right():
+    tim.right(10)
+
+def clear_screen():
+    tim.clear()
+
+screen.listen()
+
+screen.onkey(turn_left, "a")
+screen.onkey(turn_right, "d")
+screen.onkey(move_forward, "w")
+screen.onkey(move_backward, "s")
+screen.onkey(clear_screen, "space")
+
+screen.exitonclick()
